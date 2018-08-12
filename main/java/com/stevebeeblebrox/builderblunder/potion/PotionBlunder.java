a8dcdce453868e5814e1aa1028e78ca50ea4b4ad
SteveBeeblebrox <41750582+SteveBeeblebrox@users.noreply.github.com>
2018-08-11 20:06:22 -0600

    potion icon

--- main/java/com/stevebeeblebrox/builderblunder/potion/PotionBlunder.java
+++ main/java/com/stevebeeblebrox/builderblunder/potion/PotionBlunder.java
@@ -2,6 +2,7 @@ package com.stevebeeblebrox.builderblunder.potion;
 
 import com.stevebeeblebrox.builderblunder.util.Reference;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.potion.Potion;
 import net.minecraft.util.ResourceLocation;
 
@@ -12,8 +13,13 @@ public class PotionBlunder extends Potion
 	{
 		super(false, 8661504);
 		setPotionName("effect.blunder");
-		setIconIndex(2, 0);
+		setIconIndex(0, 0); //2,0
 		setRegistryName(new ResourceLocation(Reference.MOD_ID + ":" + "blunder"));
 	}
 	
+	@Override
+	public boolean hasStatusIcon() {
+		Minecraft.getMinecraft().renderEngine.bindTexture(new ResourceLocation("assets/blunder/textures/blunder.png"));
+		return true;
+	} 
 }
